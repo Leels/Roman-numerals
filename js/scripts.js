17e2ac17871ec26dba5c75236b46a0e04ac01f7d
Student <example@example.com>
2019-10-31 16:44:13 -0700

    complete roman numerals.

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,24 @@
 $(document).ready(function(){
   $("#roman-numerals").submit(function(){
     event.preventDefault()
-    var onesArr = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX']
-    var tensArr = ['X','XX','XXX','XL','L','LX','LXX','LXXX','XC']
-    var hundredsArr = ['C','CC','CCC','CD','D', 'DC', 'DCC','DCCC','CM']
-    var thousandsArr = ['M','MM','MMM']
-    var userInputArr = parseInt($("input#input").val());
-    var romanEmptyArr=[];
-
-    // for (var i = string.length -1; i >= 0; i--)
-
-
-    if (userInput <= 3999 && userInput >= 1000) {
-        var thousands = Math.floor(userInput / 1000);
-        console.log(thousands);
-        totalMs = numeralsArr[6].repeat(thousands)
-        console.log(totalMs);
-        return totalMs
-      }
+    var userInput = $("input#input").val();
+    var onesArr = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX']
+    var tensArr = ['','X','XX','XXX','XL','L','LX','LXX','LXXX','XC']
+    var hundredsArr = ['','C','CC','CCC','CD','D', 'DC', 'DCC','DCCC','CM']
+    var thousandsArr = ['','M','MM','MMM']
+    var bigArray = [onesArr, tensArr, hundredsArr, thousandsArr];
+    var inputArray= (Array.from(userInput.toString()).map(Number)).reverse();
+    var output= []
 
 
+   for (i=0; i< inputArray.length; i ++) {
+      output.push(bigArray[i][inputArray[i]]);
+}
 
-    if (isNaN(userInput) || userInput > 3999) {
-      alert("Please enter a number under 4000!");
-    }
+if (isNaN(userInput) || userInput > 3999) {
+  alert("Please enter a number under 4000!");
+}
+    $(".result").text(output.reverse().join(""));
 
 
   });
